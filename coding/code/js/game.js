648f90c6edad69a84160dc0d06e0211d3e492d68
taeeonkim <myhom43@gmail.com>
2024-06-30 19:26:30 +0900

    스프라이트이미지 애니메이션 동작 추가

--- coding/code/js/game.js
+++ coding/code/js/game.js
@@ -1,1 +1,31 @@
-/* 코드를 작성하세요 */
+const key = {
+  keyDown: {},
+  keyValue: {
+    ArrowLeft: 'left',
+    ArrowRight: 'right',
+    ArrowUp: 'up',
+    ArrowDown: 'down',
+  },
+};
+
+const windowEvent = () => {
+  window.addEventListener('keydown', (e) => {
+    console.log(e.code);
+    key.keyDown[key.keyValue[e.code]] = true;
+    console.log(key.keyDown);
+  });
+
+  window.addEventListener('keyup', (e) => {
+    key.keyDown[key.keyValue[e.code]] = false;
+    console.log(key.keyDown);
+  });
+};
+
+const init = () => {
+  windowEvent();
+};
+
+window.onload = () => {
+  // onload 찾아볼것
+  init();
+};
