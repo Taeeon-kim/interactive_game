daae3496b92f737704a5eb9a04668eeaec49a262
taeeonkim <myhom43@gmail.com>
2024-06-30 19:46:58 +0900

    좌우 변경 공격 키 이벤트 로직추가

---
+++ coding/code/js/class.js
@@ -0,0 +1,26 @@
+class Hero {
+  constructor(el) {
+    this.el = document.querySelector(el);
+    console.log(this.el);
+  }
+  keyMotion() {
+    if (key.keyDown['left']) {
+      this.el.classList.add('run');
+      this.el.classList.add('flip');
+    } else if (key.keyDown['right']) {
+      this.el.classList.add('run');
+      this.el.classList.remove('flip');
+    }
+
+    if (key.keyDown['attack']) {
+      this.el.classList.add('attack');
+    }
+
+    if (!key.keyDown['left'] && !key.keyDown['right']) {
+      this.el.classList.remove('run');
+    }
+    if (!key.keyDown['attack']) {
+      this.el.classList.remove('attack');
+    }
+  }
+}
--- coding/code/js/game.js
+++ coding/code/js/game.js
@@ -5,23 +5,29 @@ const key = {
     ArrowRight: 'right',
     ArrowUp: 'up',
     ArrowDown: 'down',
+    KeyX: 'attack',
   },
 };
+
+let hero;
 
 const windowEvent = () => {
   window.addEventListener('keydown', (e) => {
     console.log(e.code);
     key.keyDown[key.keyValue[e.code]] = true;
+    hero.keyMotion();
     console.log(key.keyDown);
   });
 
   window.addEventListener('keyup', (e) => {
     key.keyDown[key.keyValue[e.code]] = false;
+    hero.keyMotion();
     console.log(key.keyDown);
   });
 };
 
 const init = () => {
+  hero = new Hero('.hero');
   windowEvent();
 };
 
