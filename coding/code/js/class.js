92ffcc89135cf803ce1d0d8e7914342341329c23
taeeonkim <myhom43@gmail.com>
2024-06-30 21:12:03 +0900

    히어로박스 fixed,  히어로 movex, speed 초기값 추가 및 키입력에 따른 translateX값 변경, 딜레이 제거를 위해 requestAnimatiomFrame추가, 초기 이미지 로드

--- coding/code/js/class.js
+++ coding/code/js/class.js
@@ -1,16 +1,20 @@
 class Hero {
   constructor(el) {
     this.el = document.querySelector(el);
-    console.log(this.el);
+    this.movex = 0;
+    this.speed = 16;
   }
   keyMotion() {
     if (key.keyDown['left']) {
       this.el.classList.add('run');
       this.el.classList.add('flip');
+      this.movex = this.movex - this.speed;
     } else if (key.keyDown['right']) {
       this.el.classList.add('run');
       this.el.classList.remove('flip');
+      this.movex = this.movex + this.speed;
     }
+    console.log('move', this.movex);
 
     if (key.keyDown['attack']) {
       this.el.classList.add('attack');
@@ -22,5 +26,6 @@ class Hero {
     if (!key.keyDown['attack']) {
       this.el.classList.remove('attack');
     }
+    this.el.parentNode.style.transform = `translateX(${this.movex}px)`;
   }
 }
--- coding/code/js/game.js
+++ coding/code/js/game.js
@@ -15,20 +15,39 @@ const windowEvent = () => {
   window.addEventListener('keydown', (e) => {
     console.log(e.code);
     key.keyDown[key.keyValue[e.code]] = true;
-    hero.keyMotion();
+    // hero.keyMotion();
     console.log(key.keyDown);
   });
 
   window.addEventListener('keyup', (e) => {
     key.keyDown[key.keyValue[e.code]] = false;
-    hero.keyMotion();
+    // hero.keyMotion();
     console.log(key.keyDown);
+  });
+};
+
+const renderGame = () => {
+  hero.keyMotion();
+  window.requestAnimationFrame(renderGame); //60프레임 재귀함수
+  console.log('call request');
+};
+
+const loadImage = () => {
+  const preLoadImageSrc = [
+    '../../../lib/images/ninja_attack.png',
+    '../../../lib/images/ninja_run.png',
+  ];
+  preLoadImageSrc.forEach((src) => {
+    const image = new Image();
+    image.src = src;
   });
 };
 
 const init = () => {
   hero = new Hero('.hero');
+  loadImage();
   windowEvent();
+  renderGame();
 };
 
 window.onload = () => {
